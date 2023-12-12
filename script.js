adea5da233c2a6e73b21081237c98c4724f934a6
Felipe gabriel <felipedeev@gmail.com>
2023-12-12 08:27:56 -0300

    Alteração do peso da fonte nos cards, footer adicionado no site, e algumas poucas alterações banais.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 window.sr = ScrollReveal({ reset: true })
 
 sr.reveal("#menu", {duration: 1500})
+sr.reveal("#sobre", {duration: 1500})
+sr.reveal("#contato", {duration: 1500})
 sr.reveal(".slogan", {duration: 1500})
+sr.reveal(".card", {duration: 1500})
 
 window.addEventListener('scroll',()=>{
     document.querySelector("header").classList.toggle("Header_roll", window.scrollY > 0)
     document.querySelector(".op-inicio").classList.toggle("visivel", window.scrollY > 0)
 })
+
+function copygmail(){
+    navigator.clipboard.writeText("felipedeev@gmail.com").then(() =>{
+        alert("Email Copiado!")
+    })
+}
 
